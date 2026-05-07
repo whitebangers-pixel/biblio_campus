2d91708ad452e4c7eeb3c1c868c071d66c164a83
Dey Joshua <whitebangers@gmail.com>
2026-05-07 10:11:49 +0000

    Création du menu principal

---
+++ biblio_campus/main.py
@@ -0,0 +1,4 @@
+from menu import afficher_menu
+
+if __name__ == "__main__":
+    afficher_menu(
---
+++ biblio_campus/menu.py
@@ -0,0 +1,5 @@
+def afficher_menu():
+    print("=== BiblioCampus ===")
+    print("1. Gérer les livres")
+    print("2. Gérer les emprunts")
+    print("3. Voir les statistiques")
